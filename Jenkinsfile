ae1aff6f4ccf4d357b1938fc74a25bba65a7ee05
github <ghouseahammad6472@gmail.com>
2022-01-10 17:56:28 +0530

    new & Jk 2 files added for pp scm build

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+pipeline{
+    agent any
+    stages {
+         stage( 'code pull') {
+            steps {
+                git branch:" ${branch} " ,url:'https://github.com/github6472/crepo.git'
+                echo 'git pull successesful'
+            }
+        }
+        stage ('code build') {
+            steps {
+                sh 'mvn install'
+            }
+        }
+        stage ('run') {
+            steps {
+                sh ' new_script.sh '
+            }
+        }
+    }
+}
--- new_script.sh
+++ new_script.sh
@@ -5,10 +5,10 @@ i=1
 while [ "$i" -lt "$1" ]
 do
    echo "============== creating container $i =============="
-   docker run -itd --name fool_$i --hostname fool_$i -v /home/workspace/dev-data:/home/workspace/docker_data -p 806$i:8080 shaikghouse/myubuntu /bin/bash
-   echo "$(docker inspect --format '{{.NetworkSettings.Networks.bridge.IPAddress}}' cool_$i)"
-   docker cp /target/crepo.war  fool_$i:/home/dev_data/apache-tomcat-9.0.55/webapps
-     
+   docker run -itd --name ${cool_}${1} --hostname ${cool_${1}} -v /home/workspace/dev-data:/home/workspace/docker_data -p 806${1}:8080 shaikghouse/myubuntu /bin/bash
+   echo "$(docker inspect --format '{{.NetworkSettings.Networks.bridge.IPAddress}}' ${cool_}${1})"
+   docker cp /target/crepo.war  ${cool_}${1}:/home/dev_data/apache-tomcat-9.0.55/webapps
+   docker exec -itd ${cool_}${1} /home/dev_data/apache-tomcat-9.0.55/bin/startup.sh 
    i=$(($i+1))
 
 done
