6d7ff873b8c4ec2ac8896d570db0dea6714e80fd
github <ghouseahammad6472@gmail.com>
2022-01-12 19:07:44 +0530

    jk uptade & script text added

--- new_script.sh
+++ new_script.sh
@@ -8,7 +8,7 @@ do
    echo "============== creating container $i =============="
    docker run -itd --name ${cool_}$i --hostname ${cool_}$i -v /home/workspace/dev-data:/home/workspace/docker_data -p 806$i:8080 shaikghouse/myubuntu /bin/bash
    echo "$(docker inspect --format '{{.NetworkSettings.Networks.bridge.IPAddress}}' ${cool_}$i)"
-   docker cp /target/crepo.war  ${cool_}$i:/home/dev_data/apache-tomcat-9.0.55/webapps
+   docker cp /target/crepo.war  ${cool_}$i:/home/dev_data/apache-tomcat-9.0.55/webapps/
    docker exec -itd ${cool_}$i /home/dev_data/apache-tomcat-9.0.55/bin/startup.sh 
    i=$(($i+1))
 
