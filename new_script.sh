bd646c2d568ebabc53afa1bbf1e4c313e19447e2
github <ghouseahammad6472@gmail.com>
2022-01-13 12:07:46 +0530

    all changes adjusted

--- new_script.sh
+++ new_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -xe
 
 read=num
-read=cool_
+read=name
 i=1
 while [ "$i" -lt "${num}" ]
 do
    echo "============== creating container $i =============="
-   docker run -itd --name ${cool_}$i --hostname ${cool_}$i -v /home/workspace/dev-data:/home/workspace/docker_data -p 806$i:8080 shaikghouse/myubuntu /bin/bash
-   echo "$(docker inspect --format '{{.NetworkSettings.Networks.bridge.IPAddress}}' ${cool_}$i)"
-   docker cp /target/crepo.war  ${cool_}$i:/home/dev_data/apache-tomcat-9.0.55/webapps/
-   docker exec -itd ${cool_}$i:/home/dev_data/apache-tomcat-9.0.55/bin/startup.sh 
+   docker run -itd --name ${name}$i --hostname ${name}$i -v /home/workspace/dev-data:/home/workspace/docker_data -p 806$i:8080 shaikghouse/myubuntu /bin/bash
+   echo "$(docker inspect --format '{{.NetworkSettings.Networks.bridge.IPAddress}}' ${name}$i)"
+   docker cp /target/crepo.war ${name}$i:/home/dev_data/apache-tomcat-9.0.55/webapps/
+   docker exec -itd ${name}$i:/home/dev_data/apache-tomcat-9.0.55/bin/startup.sh 
    i=$(($i+1))
 
 done
