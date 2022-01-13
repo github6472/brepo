4dc73c2b88c4a5e021a3c7832413498b5a3c49dc
github <ghouseahammad6472@gmail.com>
2022-01-13 11:59:44 +0530

    all changes adjusted

--- new_script.sh
+++ new_script.sh
@@ -9,7 +9,7 @@ do
    docker run -itd --name ${cool_}$i --hostname ${cool_}$i -v /home/workspace/dev-data:/home/workspace/docker_data -p 806$i:8080 shaikghouse/myubuntu /bin/bash
    echo "$(docker inspect --format '{{.NetworkSettings.Networks.bridge.IPAddress}}' ${cool_}$i)"
    docker cp /target/crepo.war  ${cool_}$i:/home/dev_data/apache-tomcat-9.0.55/webapps/
-   docker exec -itd ${cool_}$i /home/dev_data/apache-tomcat-9.0.55/bin/startup.sh 
+   docker exec -itd ${cool_}$i:/home/dev_data/apache-tomcat-9.0.55/bin/startup.sh 
    i=$(($i+1))
 
 done
