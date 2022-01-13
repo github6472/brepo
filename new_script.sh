4048a6e1781b08d3e44e795bdcad5dea3cef4c34
github <ghouseahammad6472@gmail.com>
2022-01-13 13:04:37 +0530

    all changes adjusted

--- new_script.sh
+++ new_script.sh
@@ -2,11 +2,12 @@
 
 read=num
 read=name
+read=pn
 i=1
 while [ "$i" -lt "${num}" ]
 do
    echo "============== creating container $i =============="
-   docker run -itd --name ${name}$i --hostname ${name}$i -v /home/workspace/dev-data:/home/workspace/docker_data -p 806$i:8080 shaikghouse/myubuntu /bin/bash
+   docker run -itd --name ${name}$i --hostname ${name}$i -v /home/workspace/dev-data:/home/workspace/docker_data -p 80${pn}$i:8080 shaikghouse/myubuntu /bin/bash
    echo "$(docker inspect --format '{{.NetworkSettings.Networks.bridge.IPAddress}}' ${name}$i)"
    docker cp /target/crepo.war ${name}$i:/home/dev_data/apache-tomcat-9.0.55/webapps/
    docker exec -itd ${name}$i:/home/dev_data/apache-tomcat-9.0.55/bin/startup.sh 
